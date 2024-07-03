7329d38d77a6c581a6830c912a68ed1356892fbe
SubhamBurman <105504422+SubhamBurman@users.noreply.github.com>
2024-07-03 15:40:57 +0530

    Completed  Condition & Loops Assignment

---
+++ 19. Condition & Loops Assignment/1_Assignment_solution.js
@@ -0,0 +1,23 @@
+/*
+Q1. What are conditional statements? Explain conditional statements with syntax and examples.
+
+solution : In programming, conditional statements are fundamental building blocks that 
+allow your code to make decisions based on certain conditions. They control the flow of 
+execution, determining which parts of your code run depending on whether a specified 
+condition is true or false.
+
+Syntex :
+if (condition) {
+  // Code to execute if the condition is true
+} else {
+  // Code to execute if the condition is false
+}
+*/
+
+let isRaining = true;
+
+if (isRaining) {
+  console.log("Bring an umbrella!");
+} else {
+  console.log("Enjoy the sunshine!");
+}
---
+++ 19. Condition & Loops Assignment/5_Assignment_Solution.js
@@ -0,0 +1,10 @@
+let num =1;
+
+while (num<=24) {
+    console.log(num);
+    num++;
+}
+while (num>=1) {
+    console.log(num);
+    num--;
+}
